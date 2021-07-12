132e95a3488b5f65336d1a20572e4f253f019aa7
Marcel Pernia <marcelpm1987@gmail.com>
2021-07-12 12:21:16 -0500

    documentando el codigo

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ function llamarNumero() {
         return;
     }
 
+    // Genero un numero aleatorio entre 1 y 75
     let nuevoNumero = Math.floor(Math.random() * (75) + 1)
+    // Recupero los datos del localStorage pasandolos a json
     numerosLlamados = JSON.parse(numerosLlamados)
 
+    // Compruebo si el numero nuevo existe y si no, lo guardo en localStorage
     if (numerosLlamados.includes(nuevoNumero)) {
         numerosLlamados.length == 75 ? console.log('Todos los números ya estan presentes') : console.log(`El numero ${nuevoNumero} ya fue llamado`);
     } else {
